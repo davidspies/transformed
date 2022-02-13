7f7fa5d55bfc94f95ffb68782b571ff22124c21a
David Spies <dnspies@gmail.com>
2022-02-12 19:11:59 -0800

    Autoformat

--- src/Control/Monad/Transformed.hs
+++ src/Control/Monad/Transformed.hs
@@ -10,8 +10,8 @@ where
 import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.State
-import Control.Monad.Writer
 import Control.Monad.Trans.Control
+import Control.Monad.Writer
 import Data.Kind (Type)
 import Prelude
 
@@ -20,14 +20,8 @@ newtype Transformed t (m :: Type -> Type) a = Transformed {runTransformed :: t m
   deriving (MonadTrans, MonadTransControl) via t
 
 class MonadTransControl t => PassesWriter t where
-  fOut ::
-    Monoid w =>
-    (StT t a, w) ->
-    StT t (a, w)
-  fIn ::
-    Monoid w =>
-    StT t (a, w -> w) ->
-    (StT t a, w -> w)
+  fOut :: Monoid w => (StT t a, w) -> StT t (a, w)
+  fIn :: Monoid w => StT t (a, w -> w) -> (StT t a, w -> w)
 
 instance PassesWriter (ReaderT r) where
   fOut = id
@@ -57,8 +51,7 @@ instance
   MonadReader r (Transformed t m)
   where
   ask = lift ask
-  local fn act =
-    liftWith (\unlift -> local fn (unlift act)) >>= restoreT . return
+  local fn act = liftWith (\unlift -> local fn (unlift act)) >>= restoreT . return
   reader fn = lift $ reader fn
 
 instance
@@ -68,27 +61,18 @@ instance
   writer = lift . writer
   tell = lift . tell
   listen (act :: Transformed t m a) =
-    liftWith (\unlift -> listen (unlift act))
-      >>= restoreT . return . fOut @t @w @a
+    liftWith (\unlift -> listen (unlift act)) >>= restoreT . return . fOut @t @w @a
   pass (act :: Transformed t m (a, w -> w)) =
-    liftWith (\unlift -> pass (fIn @t @w @a <$> unlift act))
-      >>= restoreT . return
+    liftWith (\unlift -> pass (fIn @t @w @a <$> unlift act)) >>= restoreT . return
 
-instance
-  (MonadTrans t, MonadState s m, Monad (t m)) =>
-  MonadState s (Transformed t m)
-  where
+instance (MonadTrans t, MonadState s m, Monad (t m)) => MonadState s (Transformed t m) where
   get = lift get
   put = lift . put
   state = lift . state
 
-instance
-  (MonadTransControl t, MonadError e m, Monad (t m)) =>
-  MonadError e (Transformed t m)
-  where
+instance (MonadTransControl t, MonadError e m, Monad (t m)) => MonadError e (Transformed t m) where
   throwError = lift . throwError
 
   -- Loses state when an error is caught
   catchError act onError =
-    liftWith (\unlift -> unlift act `catchError` \e -> unlift $ onError e)
-      >>= restoreT . return
+    liftWith (\unlift -> unlift act `catchError` \e -> unlift $ onError e) >>= restoreT . return
